783a3ae7503f2476c5abfb8f02fadacb027cb5e6
Kalrish Bäakjen <kalrish.baakjen@gmail.com>
2016-10-27 01:09:36 +0200

    Make/engines/luajittex: Link to the official page for a description of the command line options of the LuaJIT executable

--- engines.mk/luajittex.mk
+++ engines.mk/luajittex.mk
@@ -1,5 +1,6 @@
 TEXLUA_BYTECODE_EXTENSION_luajittex := texluajitbc
 
+# See [Running LuaJIT](http://luajit.org/running.html) for a description of the options
 %.$(TEXLUA_BYTECODE_EXTENSION_luajittex) : %.lua
 ifeq ($(LUA_STRIP),y)
 	texluajitc -bst raw $< $@
