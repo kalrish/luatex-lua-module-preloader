7fc8d578147b9b21a414962421e4b8187f4bb995
Kalrish Bäakjen <kalrish.baakjen@gmail.com>
2016-10-27 00:22:51 +0200

    config.mk: Be more specific in describing what some configuration variables are for

--- config.mk
+++ config.mk
@@ -2,19 +2,18 @@
 #  either 'y' or 'n'
 LUA_STRIP := y
 
-# Which engine to use
+# Which engine to use for the tests
 #  examples: luatex, luajittex
 ENGINE := luatex
 
-# Base format to build upon
+# Base format to build upon in tests
 #  e.g.: lualatex, luajitlatex
 FORMAT := lualatex
 
-# Additional arguments to pass to the engine both in dumping formats and in
-# producing documents
+# Additional arguments to pass to the engine in tests, both in dumping formats and in producing documents
 #  e.g.: --jiton, --jithash=luajit20
 EXTRA_ENGINE_ARGUMENTS := 
 
-# Format of the final document
+# Format of the document produced in the tests
 #  e.g.: dvi, pdf
 OUTPUT_FORMAT := pdf
