ddda833c8549c40b9ac18eeddcf5d794c9b8f522
Kalrish Bäakjen <kalrish.baakjen@gmail.com>
2016-10-27 01:12:47 +0200

    Make/engines/luatex: Link to the official page for a description of the command line options of LUAC

--- engines.mk/luatex.mk
+++ engines.mk/luatex.mk
@@ -1,5 +1,6 @@
 TEXLUA_BYTECODE_EXTENSION_luatex := texluabc
 
+# See [the LUAC man page](https://www.lua.org/manual/5.2/luac.html) for a description of the options
 %.$(TEXLUA_BYTECODE_EXTENSION_luatex) : %.lua
 ifeq ($(LUA_STRIP),y)
 	texluac -s -o $@ -- $<
